d3ff17f2cd46cfd8dc4e00304687a942beaf4d39
Haixing Hu <starfish.hu@gmail.com>
2025-11-03 11:10:32 +0800

    refactor: update macro module imports
    Remove imports for deleted conditional predicate macros:
    - box_conditional_predicate
    - conditional_predicate_clone
    - conditional_predicate_conversions
    - conditional_predicate_debug_display
    - shared_conditional_predicate

--- src/predicates/macros/mod.rs
+++ src/predicates/macros/mod.rs
@@ -17,25 +17,15 @@
 //! Haixing Hu
 
 // Module declarations
-mod box_conditional_predicate;
 mod box_predicate_methods;
-mod conditional_predicate_clone;
-mod conditional_predicate_conversions;
-mod conditional_predicate_debug_display;
 mod predicate_clone;
 mod predicate_common_methods;
 mod predicate_debug_display;
-mod shared_conditional_predicate;
 mod shared_predicate_methods;
 
 // Export all macros for use within the crate
-pub(crate) use box_conditional_predicate::impl_box_conditional_predicate;
 pub(crate) use box_predicate_methods::impl_box_predicate_methods;
-pub(crate) use conditional_predicate_clone::impl_conditional_predicate_clone;
-pub(crate) use conditional_predicate_conversions::impl_conditional_predicate_conversions;
-pub(crate) use conditional_predicate_debug_display::impl_conditional_predicate_debug_display;
 pub(crate) use predicate_clone::impl_predicate_clone;
 pub(crate) use predicate_common_methods::impl_predicate_common_methods;
 pub(crate) use predicate_debug_display::impl_predicate_debug_display;
-pub(crate) use shared_conditional_predicate::impl_shared_conditional_predicate;
 pub(crate) use shared_predicate_methods::impl_shared_predicate_methods;
