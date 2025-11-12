720736ab1c63d56969b3a04b06982cbb2da5e48d
Haixing Hu <starfish.hu@gmail.com>
2025-11-12 15:03:42 +0800

    chore: Clean up unnecessary files
    - Remove coverage.lcov (large coverage report file)
    - Remove code-review.md (outdated documentation)
    - Remove test_clone.rs and test_new_macro.rs (temporary test files)

--- test_clone.rs
+++
@@ -1,7 +0,0 @@
-use prism3_function::BoxBiFunctionOnce;
-
-fn main() {
-    let func = BoxBiFunctionOnce::new(|x: &i32, y: &i32| *x + *y);
-    let cloned = func.clone(); // This should fail if not Clone
-    println!("Clone successful");
-}
